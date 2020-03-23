c3840ab759ae08260e82e9aba9be7c4ee8ff2bbb
jkaplanis <johntkaplanis@gmail.com>
2020-03-22 19:46:52 -0700

    Wrapped AJAX calls in a function that is called by the different click listeners.

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
+var APIKey = "c6e0e3fb545da16f65b665f11bf65c91";
 var citySearchHistory = JSON.parse(localStorage.getItem("searchHistory"));
 
 if (citySearchHistory === null) {
     citySearchHistory = [];
 }
-
-for (var i = 0; i < citySearchHistory.length; i++) {
-    var cityHistoryDisplay = citySearchHistory[i]
-    $("#search-history").prepend($("<button>").attr("class", "btn btn-light").text(cityHistoryDisplay));
+if (citySearchHistory.length > 0){
+    for (var i = 0; i < citySearchHistory.length; i++) {
+        var cityHistoryDisplay = citySearchHistory[i];
+        $("#search-history").prepend($("<button>").attr("class", "btn btn-light").text(cityHistoryDisplay));
+    };
+    var cityName = citySearchHistory[citySearchHistory.length - 1];
+    renderWeather(cityName);
 }
-
 
 var date = moment().format("MMM Do YYYY");
 
-var APIKey = "c6e0e3fb545da16f65b665f11bf65c91";
 
 // Listening for the click on the search button to initiate the AJAX call to the API
 $("#search-button").on("click", function (event) {
-    event.preventDefault()
+    event.preventDefault();
     var cityName = $("#input-city").val();
+    citySearchHistory.push(cityName);
+    localStorage.setItem("searchHistory", JSON.stringify(citySearchHistory));
+    $("#search-history").prepend($("<button>").attr("class", "btn btn-light").text(cityName));
+    renderWeather(cityName);
+});
+
+$("#search-history").on("click", function (event) {
+    event.preventDefault();
+    var cityName = $(event.target).text();
+    renderWeather(cityName);
+})
+
+// Here we run our AJAX call to the OpenWeatherMap API
+function renderWeather(cityName){
+    
     var weatherQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=" + APIKey;
-    // Here we run our AJAX call to the OpenWeatherMap API
     $.ajax({
         url: weatherQueryURL,
         method: "GET"
@@ -28,8 +44,6 @@ $("#search-button").on("click", function (event) {
         .then(function (response) {
             // assign values from API call to variables
             var cityDisplay = response.name;
-            citySearchHistory.push(cityDisplay);
-            localStorage.setItem("searchHistory", JSON.stringify(citySearchHistory));
             var iconCode = response.weather[0].icon;
             var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
             var humidity = response.main.humidity;
@@ -38,9 +52,9 @@ $("#search-button").on("click", function (event) {
             var lat = response.coord.lat;
             var lon = response.coord.lon;
             $("#input-city").val('');
-
+    
             var uvQueryURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
-
+    
             $.ajax({
                 url: uvQueryURL,
                 method: "GET"
@@ -49,7 +63,7 @@ $("#search-button").on("click", function (event) {
                 .then(function (uvResponse) {
                     var uvIndex = uvResponse.value
                     $("#current-uvIndex").text(uvIndex);
-
+    
                     // Assign color based on uv Index severity
                     if (uvIndex <= 4) {
                         $("#current-uvIndex").attr("class", "bg-success") //green
@@ -61,18 +75,17 @@ $("#search-button").on("click", function (event) {
                         $("#current-uvIndex").attr("class", "bg-danger") //red
                     }
                 });
-
+    
             // add text to weather fields
             $("#weather-day").text(cityDisplay + " - " + date);
             $("#weather-icon").attr("src", iconUrl);
             $("#current-temp").text(temp.toFixed(1));
             $("#current-humidity").text(humidity);
             $("#current-wind").text(windSpeed.toFixed(1));
-            $("#search-history").prepend($("<button>").attr("class", "btn btn-light").text(cityDisplay));
         });
     // query url for the 5 day forecast
     var fiveDayQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=" + APIKey;
-
+    
     $.ajax({
         url: fiveDayQueryURL,
         method: "GET"
@@ -97,4 +110,4 @@ $("#search-button").on("click", function (event) {
             }
             $("#weather-div").attr("class", "col-8 d-block")
         });
-});
+};
